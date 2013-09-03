8e7a3a213c07e1c0f02078f3b31543d65026dbf6
pivotal-is <pivotal@seaman.nyc.pivotallabs.com>
2013-09-03 15:16:36 -0400

    Initial Commit

---
+++ bot-test/BTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BTAppDelegate.h
+//  bot-test
+//
+//  Created by pivotal-is on 9/3/13.
+//  Copyright (c) 2013 Pivotal Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ bot-test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  bot-test
+//
+//  Created by pivotal-is on 9/3/13.
+//  Copyright (c) 2013 Pivotal Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BTAppDelegate class]));
+    }
+}
